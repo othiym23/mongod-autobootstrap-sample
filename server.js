7914ac8ee839f5bab6ae79287c8f65833f76be04
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-04-03 17:47:12 -0700

    Reworked startup flow and added error checking.
    Try to be as defensive as possible when bringing up mongod, including
    allowing for the possibility that a system-wide instance is already
    running, using that if possible. Also passes through all of mongod's
    output and generally made more verbose.

--- server.js
+++ server.js
@@ -4,49 +4,11 @@ var net      = require('net')
   , carrier  = require('carrier')
   , mongoose = require('mongoose')
   , spawn    = require('child_process').spawn
-  , events   = require('events')
-  , emitter  = events.EventEmitter
+  , emitter  = require('events').EventEmitter
   ;
 
 var mongoProcess;
 var mongoDir = path.join(__dirname, 'mongodata');
-
-function bootstrapMongo(callback) {
-  path.exists(mongoDir, function (exists) {
-    if (!exists) {
-      fs.mkdir(mongoDir, '0755', function (err) {
-        if (err) return callback(err);
-
-        return startMongo(callback);
-      });
-    }
-    else {
-      return startMongo(callback);
-    }
-  });
-}
-
-function startMongo(callback) {
-  var mongoOptions = ['--dbpath', mongoDir,
-                      '--nohttpinterface',
-                      '--bind_ip', 'localhost'];
-
-  mongoProcess = spawn('mongod', mongoOptions);
-
-  var mongoStdout = carrier.carry(mongoProcess.stdout);
-  var waiting = true;
-  mongoStdout.on('line', function (line) {
-    if (waiting && line.indexOf("[initandlisten] waiting for connections on port 27017") >= 0) {
-      waiting = false;
-      return callback();
-    }
-  });
-
-  mongoProcess.on('exit', function (code, signal) {
-    mongoProcess = null;
-    console.error('mongod exited with code', code, 'and signal', signal);
-  });
-}
 
 function isOpen(host, port, callback) {
     var isOpened = false;
@@ -71,28 +33,80 @@ function isOpen(host, port, callback) {
     conn.on('close', onClose);
 }
 
-process.on('exit', function () {
-  if (mongoProcess) {
-    mongoProcess.kill();
-  }
-});
+function bootstrapMongo(callback) {
+  path.exists(mongoDir, function (exists) {
+    if (!exists) {
+      fs.mkdir(mongoDir, '0700', function (err) {
+        if (err) return callback(err);
 
-isOpen('localhost', 27017, function (err, mongoStarted) {
-  console.error('err is', err, 'mongoStarted is', mongoStarted);
-  if (err) return console.error('startup error:', err);
+        return startMongo(callback);
+      });
+    }
+    else {
+      return startMongo(callback);
+    }
+  });
+}
 
-  if (!mongoStarted) {
-    bootstrapMongo(function (err) {
-      if (err) {
-        console.error('error starting mongod:', err);
-        emitter.emit('error', err);
-        return;
-      }
+function startMongo(callback) {
+  isOpen('localhost', 27017, function (err, mongoStarted) {
+    if (err) {
+      console.error('startup error:', err);
+      return callback(err);
+    }
 
-      mongoose.connect('mongodb://localhost/sample_db');
-    });
+    if (!mongoStarted) {
+      console.log('starting up service-specific mongod instance');
+
+      var mongoOptions = ['--dbpath', mongoDir,
+                          '--nohttpinterface',
+                          '--bind_ip', 'localhost'];
+
+      mongoProcess = spawn('mongod', mongoOptions);
+
+      var mongoStdout = carrier.carry(mongoProcess.stdout);
+      var waiting = true;
+      mongoStdout.on('line', function (line) {
+        console.log('[mongod] ' + line);
+
+        if (waiting && line.indexOf("[initandlisten] waiting for connections on port 27017") >= 0) {
+          console.log('mongod up and ready');
+
+          // register a handler to help ensure mongod is shut down cleanly
+          process.on('exit', function () {
+            if (mongoProcess) mongoProcess.kill();
+          });
+
+          waiting = false;
+          return callback();
+        }
+      });
+
+      var mongoStderr = carrier.carry(mongoProcess.stderr);
+      mongoStderr.on('line', function (line) {
+        console.error('[mongod] ' + line);
+      });
+
+      mongoProcess.on('exit', function (code, signal) {
+        mongoProcess = null;
+        console.error('mongod exited with code', code, 'and signal', signal);
+      });
+    }
+    else if (mongoProcess) {
+      return callback(new Error('tried to start mongod twice'));
+    }
+    else {
+      console.log('using already-running mongod instance.');
+      return callback();
+    }
+  });
+}
+
+bootstrapMongo(function (err) {
+  if (err) {
+    console.error('error starting mongod:', err);
+    return emitter.emit('error', err);
   }
-  else {
-    mongoose.connect('mongodb://localhost/sample_db');
-  }
+
+  mongoose.connect('mongodb://localhost/sample_db');
 });
